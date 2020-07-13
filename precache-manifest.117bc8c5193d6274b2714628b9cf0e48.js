8e816de36b09cbaacb4bb2804e2eb14f11433e3b
markosjanuar <markosjanuar@gmail.com>
2020-07-14 00:19:09 +0700

    Menyalin isi folder build

---
+++ precache-manifest.117bc8c5193d6274b2714628b9cf0e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b68036593da408083404",
+    "url": "/static/css/main.a82627bb.chunk.css"
+  },
+  {
+    "revision": "b68036593da408083404",
+    "url": "/static/js/main.b6803659.chunk.js"
+  },
+  {
+    "revision": "0b3217ece884079f3f11",
+    "url": "/static/js/1.0b3217ec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52990173927ca1945b87d67bb9387531",
+    "url": "/index.html"
+  }
+];
